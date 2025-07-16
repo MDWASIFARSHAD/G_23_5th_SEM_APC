353c340f4da021bd924ff34946fbada5050152ed
Md Wasif Arshad <4wasifock@gmail.com>
2025-07-16 22:57:41 +0530

    LambdaExpression_Annotation

---
+++ LEC11_16_JUL_LambdaExpression_Annotation/AnnotationExample/Test1.java
@@ -0,0 +1,16 @@
+package AnnotationExample;
+
+public class Test1 extends Test{
+	
+	@Override
+	public void lastYearTaxFile()
+	{
+		System.out.println("hello");
+	}
+	@SuppressWarnings("unchecked")
+	public void show()
+	{
+		
+	}
+
+}
