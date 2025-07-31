3585e5ed9b549ad26be95bd42d72577c6a88004b
Md Wasif Arshad <4wasifock@gmail.com>
2025-07-31 10:27:13 +0530

    Spring core Implementation thorugh XML file

---
+++ LEC20_29_JUL_Spring_Core_Implementation/src/Beans/HelloBeans.java
@@ -0,0 +1,20 @@
+package Beans;
+
+public class HelloBeans {
+	
+	private String name;
+	 private int addharNo;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAddharNo() {
+		return addharNo;
+	}
+	public void setAddharNo(int addharNo) {
+		this.addharNo = addharNo;
+	}
+
+}
---
+++ LEC20_29_JUL_Spring_Core_Implementation/src/Beans/MainClass.java
@@ -0,0 +1,17 @@
+package Beans;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		
+		
+		ApplicationContext app = new ClassPathXmlApplicationContext("spring-config.xml");
+		   HelloBeans h=(HelloBeans) app.getBean("hello");
+		   System.out.println(h.getAddharNo());
+		   System.out.println(h.getName());
+	}
+
+}
